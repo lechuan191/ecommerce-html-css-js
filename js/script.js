3946e3333d986b50f650deca05836b824d15c0bc
Le Chuan <lechuan1910@gmail.com>
2020-09-14 11:00:01 +0700

    finish index

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var menuitem = document.getElementById("menuitem");
 menuitem.style.maxHeight = "0px";
 
 img.onclick = function(){
-    if(menuitem.style.maxHeight = "0px"){
+    if(menuitem.style.maxHeight == "0px"){
         menuitem.style.maxHeight = "200px";
     }
     else{
